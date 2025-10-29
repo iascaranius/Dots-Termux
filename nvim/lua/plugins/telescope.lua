d22bd2cf5ef24609f6ae73c71044b8e32dcc299f
Jaskaran <jsskrn007@gmail.com>
2025-10-29 15:16:54 +0530

    minor

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -23,6 +23,9 @@ return {
 
       vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files" })
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Live grep" })
+
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'List buffers' })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Search help' })
     end,
   },
 }
