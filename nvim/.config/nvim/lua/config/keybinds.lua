e2f0720ff16ec05b71e95078c22de8512963aff0
Jaskaran <jsskrn007@gmail.com>
2026-05-24 22:05:27 +0530

    Probably works

--- nvim/.config/nvim/lua/config/keybinds.lua
+++ nvim/.config/nvim/lua/config/keybinds.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-
+vim.keymap.set("n", "<leader>sr", function()
+    require("grug-far").open()
+end)
 -- Normal mode: Space + e to open file explorer
 -- vim.keymap.set("n", "<leader>e", ":Ex<CR>", { desc = "Open file explorer" })
